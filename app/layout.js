134f9d00c29911e413ef7a934a0dc9ae1eebae56
Ayokunnumi1 <omololuayk@gmail.com>
2025-08-13 01:07:40 +0100

    Use script component for google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,30 @@
+import RootLayout from "./RootLayout";
+import Script from "next/script";
+
 export const metadata = {
-//   title: {
-//     default: "IGP"
-//   },
   icons: {
     icon: "/images/itan-favicon.png",
   },
 };
 
-
-import RootLayout from "./RootLayout";
-
 export default function Layout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SGQ45D1RLL"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SGQ45D1RLL');
+          `}
+        </Script>
+      </head>
       <body className="bg-gray-100">
         <RootLayout>{children}</RootLayout>
       </body>
